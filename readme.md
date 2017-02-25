f672c90635c2429c8e995691a7f75065e684f08a
Adam Pearce <1wheel@gmail.com>
2017-02-25 02:53:54 +0000

    adds notes

---
+++ readme.md
@@ -0,0 +1,9 @@
+pip install --upgrade https://github.com/msuozzo/Lector/tarball/master
+
+
+how to copy folder after usb drive inserted
+- https://www.macissues.com/2014/07/16/how-to-set-up-and-use-folder-actions-on-your-mac/
+- http://www.jbmurphy.com/2011/07/15/os-x-running-a-script-when-a-usb-drive-is-inserted/
+- http://apple.stackexchange.com/questions/8090/how-to-run-applescript-on-disk-mount
+- http://macscripter.net/viewtopic.php?id=24748
+- http://www.launchd.info/
--- update.py
+++ update.py
@@ -1,9 +1,13 @@
 import lector
+import config
 
-api = lector.KindleCloudReaderAPI('my_amazon_username', 'my_amazon_password')
+
+api = lector.KindleCloudReaderAPI(config.username, config.password)
 my_library = api.get_library_metadata()
 book = my_library[0]
 book_progress = api.get_book_progress(book.asin)
 _, current_page, last_page = book_progress.page_nums
+start_pos, current_pos, last_pos = book_progress.positions
 
 print 'Currently reading %s (Page %d of %d)' % (book.title, current_page, last_page)
+print 'Currently reading %s (Pos %d, %d of %d)' % (book.title, start_pos, current_pos, last_pos)
