a3cba7091f13572d3bfcab7d2f2f111e8c80869e
Adam Pearce <1wheel@gmail.com>
2017-03-07 23:45:00 -0500

    tidies top chart

--- public/script.js
+++ public/script.js
@@ -1,4 +1,10 @@
+console.clear()
 var ƒ = d3.f
+
+var isbnToName = {
+  B00H7WPC4S: 'History of Portugal', 
+  B005WBGNZS: 'Straight Man'
+}
 
 d3.json('data.json', function(err, res){
   data = res
@@ -7,19 +13,26 @@ d3.json('data.json', function(err, res){
     d.pos = d.position.pos
     d.isbn = d.contentReference.asin
   })
-  console.log(data[0])
+  data = data.filter(d => d.date < new Date(2017, 2, 0))
 
-  byBook = d3.nestBy(data, ƒ('asin'))
+  byBook = d3.nestBy(data, ƒ('isbn'))
+  byBook.forEach(d => {
+    d.last = _.last(d)
+    d.title = isbnToName[d.key] || ''
+    d.titleSpans = d3.wordwrap(d.title, 15)
+  })
 
-  var sel = d3.select('body').html('')
-  var c = d3.conventions({parentSel: sel, margin: {left: 100}})
+  var sel = d3.select('#kindle-slope').html('')
+  var c = d3.conventions({parentSel: sel, width: 750, height: 150, margin: {left: 80*1}})
 
   c.x = d3.scaleTime()
     .domain(d3.extent(data, ƒ('date')))
     .range(c.x.range())
+
   c.y.domain([0, d3.max(data, ƒ('pos'))])
 
-  c.xAxis.scale(c.x).tickFormat(d3.timeFormat("%m/%d"))
+  c.yAxis.ticks(5)
+  c.xAxis.scale(c.x).tickFormat(d3.timeFormat("%m/%d")).ticks(5)
   c.drawAxis()
 
    line = d3.line()
@@ -34,6 +47,41 @@ d3.json('data.json', function(err, res){
       stroke: '#000',
     })
 
+  var lastSel = c.svg.appendMany(byBook, 'g')
+    .filter(d => d.last.pos > 1000)
+    .translate(d => [c.x(d.last.date), c.y(d.last.pos)])
+
+  lastSel.append('circle')
+    .at({r: 3, stroke: '#000', fill: '#fff', cy: -4})
+
+  lastSel.append('text')
+    .at({textAnchor: 'middle', y: d => d.titleSpans.length*-13})
+    .tspans(ƒ('titleSpans'))
+})
+
+
+
+d3.csv('lookups.csv', function(err, res){
+  words = res
+
+  words.forEach(function(d){
+    d.word = d.word_key.split(':')[1]
+    d.date = new Date(+d.timestamp)
+    d.hour = d.time.getHours()
+  })
+
+
+  var sel = d3.select('#kindle-slope').html('')
+  var c = d3.conventions({parentSel: sel, width: 750, height: 150, margin: {left: 80*1}})
+
+  c.x = d3.scaleTime()
+    .domain(d3.extent(words, ƒ('date')))
+    .range(c.x.range())
+
+  c.y.domain([0, 23])
+  
 
 
 })
+
+
