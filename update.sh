4170d75cfe8ac938a4e80b72b8f14e81f0ae2f5d
Adam Pearce <1wheel@gmail.com>
2019-10-21 22:48:19 -0400

    less errors

--- update.sh
+++ update.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
-cp -R /Volumes/Kindle/system/userannotlogsDir/* /Users/adam/Desktop/kindle-tracker/logs/
+if [ -d /Volumes/Kindle/ ]; then
+  # Control will enter here if $DIRECTORY exists.
+  cp -R /Volumes/Kindle/system/userannotlogsDir/* /Users/adam/Desktop/kindle-tracker/logs/
+  node /Users/adam/Desktop/kindle-tracker/parse.js
 
-# uncomment to auto upload
-# git commit 'data-update' 
-# git push
+  # uncomment to auto upload
+  # git commit 'data-update' 
+  # git push
+fi
 
-node /Users/adam/Desktop/kindle-tracker/parse.js
+
+
+
